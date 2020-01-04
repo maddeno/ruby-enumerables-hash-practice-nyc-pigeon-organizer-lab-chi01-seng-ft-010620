11207c93baa1018f6e996b142097ae145dbb2fa3
Olivia Madden <madden.livi@gmail.com>
2020-01-04 03:05:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,6 @@
 def nyc_pigeon_organizer(data)
-  puts data
-  pigeon_name_genders = data[:gender]
-  pigeon_name_array = []
-  puts pigeon_name_genders[:male]
-  
+  male_pigeons = data[:gender].map do |gender|
+    
+  end
   
 end
