5a3afabf7d46a473ffd212b14b3b1aded2070be0
Olivia Madden <madden.livi@gmail.com>
2020-01-04 03:31:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_names_array = data[:gender][:male] + data[:gender][:female]
+  pigeon_list = {}
+  pigeon_names_array.each do |name|
+    pigeon_data = {
+      color: [],
+      gender: [],
+      lives: []
+    }
+    data[:color].each do |color_name, values|
+      if values.include?(name)
+        pigeon_data[:color].push(color_name.to_s)
+      end
+    end
+    data[:gender].each do |gender_name, values|
+      if values.include?(name)
+        pigeon_data[:gender].push(gender_name.to_s)
+      end
+    end
+    data[:lives].each do |lives_name, values|
+      if values.include?(name)
+        pigeon_data[:lives].push(lives_name.to_s)
+      end
+    end
+    pigeon_list[name] = pigeon_data
+  end
+  pigeon_list
 end
